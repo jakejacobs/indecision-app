0a6ed38696d4b34e512d5f5eb51404fb8354994b
Jake Jacobs <jacky4uonly2009@gmail.com>
2021-07-09 02:04:47 +0530

    Initial setup

---
+++ public/scripts/app.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var template = /*#__PURE__*/React.createElement("p", null, "Indecison App");
+var appRoot = document.querySelector('#app');
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,4 @@
+const template = <p>Indecison App</p>;
+const appRoot = document.querySelector('#app');
+
+ReactDOM.render(template, appRoot);
