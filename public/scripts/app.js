36777a582bbb64d194ab743c2120f70004174532
Jake Jacobs <jacky4uonly2009@gmail.com>
2021-07-10 17:39:22 +0530

    conditional jsx

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,24 @@
 "use strict";
 
-var template = /*#__PURE__*/React.createElement("p", null, "Indecison App");
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
+};
+var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, app.title), app.subtitle && /*#__PURE__*/React.createElement("p", null, app.subtitle), app.options.length > 0 ? 'Here are you options' : 'No options', /*#__PURE__*/React.createElement("ol", null, /*#__PURE__*/React.createElement("li", null, "Item one"), /*#__PURE__*/React.createElement("li", null, "Item two"))); //////////////////////////////////////////////////////////////////////////
+
+var user = {
+  name: 'Jake Jacobs',
+  age: 28,
+  location: 'Guntur'
+};
+
+var getLocation = function getLocation(location) {
+  if (location) {
+    return /*#__PURE__*/React.createElement("p", null, "Location: ", location);
+  }
+};
+
+var template2 = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, user.name ? user.name : 'Anonymous'), user.age && user.age >= 18 && /*#__PURE__*/React.createElement("p", null, "Age: ", user.age), getLocation(user.location));
 var appRoot = document.querySelector('#app');
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,4 +1,41 @@
-const template = <p>Indecison App</p>;
+const app = {
+	title: 'Indecision App',
+	subtitle: 'Put your life in the hands of a computer',
+	options: [ 'One', 'Two' ]
+};
+
+const template = (
+	<div>
+		<h1>{app.title}</h1>
+		{app.subtitle && <p>{app.subtitle}</p>}
+		{app.options.length > 0 ? 'Here are you options' : 'No options'}
+		<ol>
+			<li>Item one</li>
+			<li>Item two</li>
+		</ol>
+	</div>
+);
+
+//////////////////////////////////////////////////////////////////////////
+const user = {
+	name: 'Jake Jacobs',
+	age: 28,
+	location: 'Guntur'
+};
+
+const getLocation = (location) => {
+	if (location) {
+		return <p>Location: {location}</p>;
+	}
+};
+
+const template2 = (
+	<div>
+		<h1>{user.name ? user.name : 'Anonymous'}</h1>
+		{user.age && user.age >= 18 && <p>Age: {user.age}</p>}
+		{getLocation(user.location)}
+	</div>
+);
 const appRoot = document.querySelector('#app');
 
 ReactDOM.render(template, appRoot);
