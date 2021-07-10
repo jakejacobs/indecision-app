6286b248db8106b95c0343f78319ebfa2ceaf780
Jake Jacobs <jacky4uonly2009@gmail.com>
2021-07-10 19:34:33 +0530

    forms and input

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,22 +3,34 @@
 var app = {
   title: 'Indecision App',
   subtitle: 'Put your life in the hands of a computer',
-  options: ['One', 'Two']
-};
-var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, app.title), app.subtitle && /*#__PURE__*/React.createElement("p", null, app.subtitle), app.options.length > 0 ? 'Here are you options' : 'No options', /*#__PURE__*/React.createElement("ol", null, /*#__PURE__*/React.createElement("li", null, "Item one"), /*#__PURE__*/React.createElement("li", null, "Item two"))); //////////////////////////////////////////////////////////////////////////
-
-var user = {
-  name: 'Jake Jacobs',
-  age: 28,
-  location: 'Guntur'
+  options: []
 };
 
-var getLocation = function getLocation(location) {
-  if (location) {
-    return /*#__PURE__*/React.createElement("p", null, "Location: ", location);
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+  var option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    render();
   }
 };
 
-var template2 = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, user.name ? user.name : 'Anonymous'), user.age && user.age >= 18 && /*#__PURE__*/React.createElement("p", null, "Age: ", user.age), getLocation(user.location));
 var appRoot = document.querySelector('#app');
-ReactDOM.render(template, appRoot);
+
+var render = function render() {
+  var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, app.title), app.subtitle && /*#__PURE__*/React.createElement("p", null, app.subtitle), app.options.length > 0 ? 'Here are you options' : 'No options', /*#__PURE__*/React.createElement("ol", null, app.options.map(function (option) {
+    return /*#__PURE__*/React.createElement("li", {
+      key: option
+    }, option);
+  })), /*#__PURE__*/React.createElement("form", {
+    onSubmit: onFormSubmit
+  }, /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "option"
+  }), /*#__PURE__*/React.createElement("button", null, "Add Option")));
+  ReactDOM.render(template, appRoot);
+};
+
+render();
